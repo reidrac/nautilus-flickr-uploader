8828b1187acc3edbed790ec9903a258f03fd0cac
reidrac <jjm@usebox.net>
2012-02-11 16:01:01 +0000

    updated strings

--- UI/Account.glade.h
+++ UI/Account.glade.h
@@ -1,5 +1,6 @@
 char *s = N_("<b>This program requires your authorization before it can upload your photos to Flickr.</b>");
 char *s = N_("<small>You must be connected to the Internet in order to authorize this program.</small>");
-char *s = N_("Authorizing is a simple process wich takes place in your web browser. When you're finished, return to this window to complete authorization and begin using <i>nautilus-flickr-uploader</i>.");
+char *s = N_("Authorizing is a simple process wich takes place in your web browser. When you're finished, return to this window to complete authorization entering the verification code.");
 char *s = N_("Nautilus Flickr Uploader - Flickr Authentication");
+char *s = N_("Verification Code:");
 char *s = N_("_Next");
